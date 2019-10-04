d83d2e1908df834ee7e4f72a12e797dcb91ca2b0
Markus Dresch <markus@dresch.com>
2019-10-04 11:14:36 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -3,7 +3,11 @@ provider "kubernetes" {
 }
 
 resource "null_resource" "custom" {
+  # download kubectl
   provisioner "local-exec" {
-    command = "kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment.yaml"
+    command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+  }
+  provisioner "local-exec" {
+    command = "./kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment.yaml"
   }
 }
