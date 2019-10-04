760a387540cfbbe3de4ca488be7fa072c43358b1
Markus Dresch <markus@dresch.com>
2019-10-04 12:39:24 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -3,11 +3,18 @@ provider "kubernetes" {
 }
 
 resource "null_resource" "custom" {
+  # change trigger to run every time
+  triggers {
+    build_number = "${timestamp()}"
+  }
+
   # download kubectl
   provisioner "local-exec" {
-    command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && ls -lah"
   }
+
+  # run kubectl
   provisioner "local-exec" {
-    command = "./kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment.yaml"
+    command = "./kubectl apply -f deployment.yaml"
   }
 }
